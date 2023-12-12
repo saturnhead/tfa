99f4238de7b6eb183e08d93f0080cc2a7422352d
Flavius <flaviuscdinu93@gmail.com>
2023-12-12 14:37:31 +0200

    Added another instance

--- tf/main.tf
+++ tf/main.tf
@@ -44,6 +44,10 @@ locals {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t3.micro"
     }
+    instance6 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t3.micro"
+    }
   }
 }
 
