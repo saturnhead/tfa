b7192ff7b7eed18c9f38e7dc7909421488cb7c13
Flavius Dinu <57911064+flavius-dinu@users.noreply.github.com>
2024-05-24 16:20:16 +0300

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -26,7 +26,7 @@ locals {
   instances = {
     instance1 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t3.micro"
+      instance_type = "t2.micro"
     }
     instance2 = {
       ami           = data.aws_ami.ubuntu.id
